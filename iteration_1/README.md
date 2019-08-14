29093cb4f94c7d3ab0a721cc1bbd077307ee950f
LaetitiaDaCosta <laetitiadacosta@mailoo.org>
2019-08-14 09:11:46 +0200

    retour du GitIgnore

--- iteration_1/README.md
+++
@@ -1,5 +0,0 @@
-# python-warrior-base
-Python Warrior Base Game
-
-## Requirements
-See `requirements.txt`
--- iteration_2/src/game/caissesurprise.py
+++ iteration_2/src/game/caissesurprise.py
@@ -16,7 +16,10 @@ class CaisseSurprise(object):
     def get_list_caisses_surprises(cls):
         return [cls("Arc", "\033[32m🏹\033[0m", 3, ajout_pt_attack=1, ajout_pt_vie=0),
                 cls("Potion de vie mineure", "\033[32m🍾\033[0m", 7, ajout_pt_attack=0, ajout_pt_vie=1),
+                cls("boules de feu", "\033[32m🔥\033[0m", 8, ajout_pt_attack=7, ajout_pt_vie=0),
                 cls("grande potion de vie", "\033[32m🍾\033[0m", 11,ajout_pt_attack= 0, ajout_pt_vie=5),
+                cls("Boule de feu", "\033[32m🔥\033[0m", 9, ajout_pt_attack=7, ajout_pt_vie=0),
+                cls("Eclair", "\033[32m🌩️\033[0m", 13, ajout_pt_attack=2, ajout_pt_vie=0),
                 cls("Potion de vie standard", "\033[32m🍾\033[0m", 15, ajout_pt_attack=0, ajout_pt_vie=2),
                 cls("Massue", "\033[32m🔨\033[0m", 8, ajout_pt_attack=3, ajout_pt_vie=0),
                 cls("Epée", "\033[32m⚔\033[0m", 10, ajout_pt_attack=5, ajout_pt_vie=0)]
