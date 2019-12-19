53fad9dce957ec9c5490b2d0ae9c5339eda7ada4
dorianpelat <dorian.pelat@gmail.com>
2019-12-19 16:06:04 +0100

    iteration_5: creation de carte via fichier .json

--- iteration_3/src/game/coffre.py
+++ iteration_3/src/game/coffre.py
@@ -1,4 +1,5 @@
 from .base import BaseName, BaseCompetence
+import json
 
 
 class Coffre(BaseName, BaseCompetence):
@@ -8,37 +9,14 @@ class Coffre(BaseName, BaseCompetence):
         BaseCompetence.__init__(self, image, life, attack_level)
         self.case = case
 
-
     @classmethod
     def get_list_coffre(cls):
-        list_coffre = [
-            cls("Arc", "\033[34m🏹\033[0m", 2, 0, 1),
-            cls("Arc", "\033[34m🏹\033[0m", 11, 0, 1),
-            cls("Arc", "\033[34m🏹\033[0m", 14, 0, 1),
-            cls("Arc", "\033[34m🏹\033[0m", 19, 0, 1),
-            cls("Arc", "\033[34m🏹\033[0m", 26, 0, 1),
-            cls("Massue", "\033[37m🔨\033[0m", 5, 0, 3),
-            cls("Massue", "\033[37m🔨\033[0m", 22, 0, 3),
-            cls("Massue", "\033[37m🔨\033[0m", 38, 0, 3),
-            cls("Epée", "\033[37m⚔️\033[0m", 42, 0, 5),
-            cls("Epée", "\033[37m⚔️\033[0m️", 53, 0, 5),
-            cls("Eclair", "\033[33m⚡️\033[0m️️", 1, 0, 2),
-            cls("Eclair", "\033[33m⚡️\033[0m️️", 4, 0, 2),
-            cls("Eclair", "\033[33m⚡️\033[0m️️", 8, 0, 2),
-            cls("Eclair", "\033[33m⚡️\033[0m", 17, 0, 2),
-            cls("Eclair", "\033[33m⚡️\033[0m", 23, 0, 2),
-            cls("Boules de feu", "\033[37m💥\033[0m", 48, 0, 7),
-            cls("Boules de feu", "\033[37m💥\033[0m", 49, 0, 7),
-            cls("Potion  de vie mineures", "\033[31m❤\033[0m️️️️", 7, 1, 0),
-            cls("Potion  de vie mineures", "\033[31m❤\033[0m️️️️", 13, 1, 0),
-            cls("Potion  de vie mineures", "\033[31m❤\033[0m️️️️", 28, 1, 0),
-            cls("Potion  de vie mineures", "\033[31m❤\033[0m", 29, 1, 0),
-            cls("Potion  de vie mineures", "\033[31m❤\033[0m️️️️", 33, 1, 0),
-            cls("Potion de vie standard", "\033[31m💕\033[0m️️️️", 31, 2, 0),
-            cls("Potion de vie standard", "\033[31m💕\033[0m️️️", 39, 2, 0),
-            cls("Potion de vie standard", "\033[31m💕\033[0m️️️️", 43, 2, 0),
-            cls("Grande potion de vie", "\033[31m💖\033[0m", 41, 5, 0),
-        ]
+        with open('src/res/donjons_map.json') as json_file:
+            data = json.load(json_file)
+        list_coffre = []
+        for coffre in data["coffres"]:
+            list_coffre.append(cls(coffre["name"], coffre["image"], coffre["case"], coffre["life"], coffre["attack_level"]))
+
         return list_coffre
 
     def __repr__(self):
--- iteration_3/src/game/ennemi.py
+++ iteration_3/src/game/ennemi.py
@@ -1,4 +1,5 @@
 from .base import BaseName, BaseCompetence
+import json
 
 
 class Ennemi(BaseName, BaseCompetence):
@@ -10,32 +11,12 @@ class Ennemi(BaseName, BaseCompetence):
 
     @classmethod
     def get_list_ennemi(cls):
-        list_ennemi = [
-            cls("Dragon", "\033[32m🐉\033[0m", 45, 15, 4),
-            cls("Dragon", "\033[32m🐉\033[0m", 52, 15, 4),
-            cls("Dragon", "\033[32m🐉\033[0m", 56, 15, 4),
-            cls("Dragon", "\033[32m🐉\033[0m", 62, 15, 4),
-            cls("Sorciers", "\033[36m🌒\033[0m", 10, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 20, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 25, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 32, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 35, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 36, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 37, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 40, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 44, 9, 2),
-            cls("Sorciers", "\033[36m🌒\033[0m", 47, 9, 2),
-            cls("Gobelins", "\033[35m🧟\033[0m", 3, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 6, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 9, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 12, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 15, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 18, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 21, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 24, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 27, 6, 1),
-            cls("Gobelins", "\033[35m🧟\033[0m", 30, 6, 1),
-        ]
+        with open('src/res/donjons_map.json') as json_file:
+            data = json.load(json_file)
+        list_ennemi = []
+        for enemy in data["enemies"]:
+            list_ennemi.append(cls(enemy["name"],enemy["image"],enemy["case"], enemy["life"], enemy["attack_level"]))
+
         return list_ennemi
 
     def __repr__(self):
